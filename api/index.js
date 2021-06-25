e3748a26960aa48cc5bb9bb73c95f445016e04fa
Yifang Ma <yifang@nearprotocol.com>
2021-06-25 16:27:27 +0800

    add partner

--- api/index.js
+++ api/index.js
@@ -1,15 +1,37 @@
-const { Router } = require('express')
-const {getQueryResultFromIndexer} = require('./result')
-const { validationErrorHandler } = require('./error')
+const { Router } = require('express');
+const { getQueryResultFromIndexer } = require('./result');
+const { validationErrorHandler } = require('./error');
+const {
+  queryForPartnerTransactions,
+  queryForPartnerUniqueUserAmount,
+} = require('./partner');
 
-const router = new Router()
+const router = new Router();
 
 const getResult = async (req, res) => {
-  query = req.body.query
-  result = await getQueryResultFromIndexer(query)
-  res.json(result)
-}
+  query = req.body.query;
+  result = await getQueryResultFromIndexer(query);
+  res.json(result);
+};
 
-router.post('/',validationErrorHandler, getResult)
+const getResultForPartnerTransaction = async (req, res) => {
+  (timestamp = req.body.timestamp), (partner_list = req.body.partner_list);
+  result = await queryForPartnerTransactions(timestamp, partner_list);
+  res.json(result);
+};
 
-module.exports = router
+const getResultForPartnerUser = async (req, res) => {
+  (timestamp = req.body.timestamp), (partner_list = req.body.partner_list);
+  result = await queryForPartnerUniqueUserAmount(timestamp, partner_list);
+  res.json(result);
+};
+
+router.post('/', validationErrorHandler, getResult);
+router.post(
+  '/partner-transaction',
+  validationErrorHandler,
+  getResultForPartnerTransaction
+);
+router.post('/partner-user', validationErrorHandler, getResultForPartnerUser);
+
+module.exports = router;
